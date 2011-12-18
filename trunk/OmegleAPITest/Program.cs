43982d81c4d804043ce6819bfa9351e72e4830ff
naarkie <naarkie@cf990750-13d6-09c1-c125-463a0a275675>
2011-12-18 07:53:36 +0000

    First commit

---
+++ trunk/OmegleAPITest/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using dotOmegle;
+
+namespace OmegleAPITest
+{
+    internal class Program
+    {
+        public static Omegle omegle = new Omegle();
+
+        private static void Main(string[] args)
+        {
+            omegle.MessageReceived += new MessageReceivedEvent(omegle_MessageReceived);
+            omegle.StrangerDisconnected += new EventHandler(omegle_StrangerDisconnected);
+            omegle.StrangerTyping += new EventHandler(omegle_StrangerTyping);
+            omegle.WaitingForPartner += new EventHandler(omegle_WaitingForPartner);
+
+            omegle.continueRestarts = true;
+            omegle.omegleMode = true;
+            Console.WriteLine("Connecting...");
+            omegle.Start();
+        }
+
+        private static void omegle_WaitingForPartner(object sender, EventArgs e)
+        {
+            Console.WriteLine("Waiting for partner...");
+        }
+
+        private static void omegle_StrangerTyping(object sender, EventArgs e)
+        {
+            Console.WriteLine("Stranger typing...");
+        }
+
+        private static void omegle_StrangerDisconnected(object sender, EventArgs e)
+        {
+            Console.WriteLine("Stranger is disconnected. Restarting main loop.");
+            omegle.MainLoop();
+            return;
+            /*Console.WriteLine("Stranger is disconnected. Closing connection.");
+             *omegle.Close();
+             *System.Environment.Exit(0);
+             */
+        }
+
+        private static void omegle_MessageReceived(object sender, MessageReceivedArgs e)
+        {
+            Console.WriteLine("Message received: " + e.message + "\n");
+            Console.Write(">");
+            string answer = Console.ReadLine();
+            omegle.SendMessage(answer);
+        }
+    }
+}
