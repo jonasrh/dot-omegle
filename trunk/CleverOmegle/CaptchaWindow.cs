fe80c85de57596f14d5bdd02bbd8b3ae02a5ec6a
naarkie <naarkie@cf990750-13d6-09c1-c125-463a0a275675>
2011-12-22 08:18:31 +0000

    Added captcha support, and the user can choose whether the bot initiates conversations or not. Captcha support untested because I cannot get Omegle to send me a captcha request.

---
+++ trunk/CleverOmegle/CaptchaWindow.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace CleverOmegle
+{
+    public partial class CaptchaWindow : Form
+    {
+        public CaptchaWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void CaptchaWindow_Load(object sender, EventArgs e)
+        {
+            captchaPictureBox.WaitOnLoad = true;
+            captchaPictureBox.LoadAsync(Program.captchaURL);
+        }
+
+        private void CaptchaWindow_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            e.Cancel = true;
+            this.Hide();
+        }
+    }
+}
